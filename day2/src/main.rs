1301e7506b5fc37957566576d1f36b1367651e28
BazzaCipher <33623276+BazzaCipher@users.noreply.github.com>
2023-12-23 20:14:19 +1100

    Day 2 Part 2 All

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use strum_macros::EnumString;
 use std::str::FromStr;
 
-#[derive(Hash, PartialEq, Eq, EnumString)]
+#[derive(Clone, Hash, PartialEq, Eq, EnumString)]
 enum Colour {
     #[strum(serialize="red")]
     Red,
@@ -12,42 +12,50 @@ enum Colour {
     Green
 }
 
-
-
 fn main() {
     let input = include_str!("../input");
-    let max: HashMap<Colour, u32> = HashMap::from([
+    let max: HashMap<Colour, u64> = HashMap::from([
         (Colour::Red, 12),
         (Colour::Blue, 14),
         (Colour::Green, 13),
     ]);
 
-    let out: u32 = input.lines()
+    let out: u64 = input.lines()
         .map(|line| {
-            let gameid_sets: Vec<_> = line.split(':').take(2).collect();
+            let game = line.split(':').nth(1).unwrap();
 
-            if is_possible(gameid_sets[1], &max) { read_gameid(gameid_sets[0]) } else { 0 }
+            game.split_terminator(&[';', ','])
+                .map(num_colour)
+                .fold(HashMap::new(), |mut acc, e| {
+                    // Too lazy for complicated efficiency
+                    acc.insert(e.1.clone(), u64::max(e.0, acc.get(&e.1).map(|e| *e).unwrap_or_default()));
+                    acc
+                })
+                .values()
+                .copied()
+                .collect()
         })
+        .map(|values: Vec<_>| values.into_iter().reduce(|acc, e| acc * e).unwrap())
         .sum();
 
     println!("{out}");
 }
 
-fn read_gameid(fixed_str: &str) -> u32 {
-    fixed_str.strip_prefix("Game ").unwrap().parse().expect("Was valid")
-}
+// fn read_gameid(fixed_str: &str) -> u32 {
+//     fixed_str.strip_prefix("Game ").unwrap().parse().expect("Was valid")
+// }
 
-fn is_possible(all_sets: &str, max: &HashMap<Colour, u32>) -> bool {
-    all_sets.split(';')
-        .all(|set| {
-            set.split(',')
-                .map(num_colour)
-                .all(|(count, colour)| max.get(&colour).unwrap() >= &count)
-        })
+// fn is_possible(all_sets: &str, max: &HashMap<Colour, u32>) -> bool {
+//     all_sets.split(';')
+//         .all(|set| {
+//             set.split(',')
+//                 .map(num_colour)
+//                 .all(|(count, colour)| max.get(&colour).unwrap() >= &count)
+//         })
+//
+// }
 
-}
-
-fn num_colour(input: &str) -> (u32, Colour) {
+fn num_colour(input: &str) -> (u64, Colour) {
     let words: Vec<_> = input.split_whitespace().take(2).collect();
     (words[0].parse().unwrap(), Colour::from_str(words[1]).unwrap())
 }
