d126a2742128f9d725ba6fa118e02f7efcbb93f6
BazzaCipher <33623276+BazzaCipher@users.noreply.github.com>
2023-12-25 15:47:12 +1100

    Day 4 Part 2 All

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,24 +1,47 @@
-use std::collections::HashMap;
-
 fn main() {
+    let input = card_input();
+
+    let used_card_count = simulate_card(&input);
+
+    println!("{:?}", used_card_count);
+}
+
+// Expects a vector of cards that denotes how many following cards must be
+// copied once.
+fn simulate_card(cards: &Vec<u32>) -> u32 {
+    let numcards = cards.len();
+    let mut onhand_cards = vec![1;numcards];
+
+    for ind in 0..numcards {
+        // Must choose following cards
+        for cd in 1..=cards[ind] {
+            if ind + cd as usize >= numcards { continue }
+            onhand_cards[ind + cd as usize] += onhand_cards[ind];
+        }
+    }
+
+    println!("{:?}", onhand_cards);
+    onhand_cards.iter().sum()
+}
+
+fn card_input() -> Vec<u32> {
     let input = include_str!("../input");
 
     // Card 1 will be at index 0, etc.
-    let out: u64 = input.lines()
+    let out: Vec<u32> = input.lines()
         .map(|x| x.split_once(':')
              .expect("Can strip prefix 'Card'").1
              .split_once('|')
              .unwrap()
         )
         .map(|(winners, numbers)| {
-            let mut win_num: isize = -1;
+            let mut win_num: u32 = 0;
             let winners: Vec<&str> = winners.split_whitespace().collect();
             for num in numbers.split_whitespace() {
                 if winners.contains(&num) { win_num += 1 }
             }
-            if win_num >= 0 { 2_u64.pow(win_num as u32) } else { 0 }
-        })
-        .sum();
+            win_num
+        }).collect();
 
-    println!("{:?}", out);
+    out
 }
