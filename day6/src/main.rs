14b79c6a4e9e94cfbb0258fd1a08003dd16772a4
BazzaCipher <33623276+BazzaCipher@users.noreply.github.com>
2023-12-26 23:09:17 +1100

    Day 6 Part 2 Some: Attempted but overflowed

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,38 +1,38 @@
+use num_traits::{Float, Zero, Signed, cast::FromPrimitive};
+use std::ops::Mul;
+
 fn main() {
     let input = input();
 
     // From funny maths, we find that time of release is half of time max
-    let out: Vec<f32> = input.map(|(time, dist)| {
-        let (left, right) = quadratic_roots(-1., time as f32, -(dist as f32));
+    let (left, right) = quadratic_roots::<f64>(-1., input.0 as f64, -(input.1 as f64));
+    let out: f64 = (right - 1.).ceil() - (left + 1.).floor() + 1.;
 
-        (right - 1.).ceil() - (left + 1.).floor() + 1.
-    }).collect();
-
-    println!("{:?}", out.iter().product::<f32>());
+    println!("{:?}", out);
 }
 
-fn input() -> impl Iterator<Item = (u32, u32)> {
+fn input() -> (u32, u32) {
     let input = include_str!("../input");
 
     let [times, dists] = input
         .lines()
         .map(|line| line.split_once(':').unwrap().1)
+        .map(|nums| nums.split_whitespace().map(str::parse::<u32>).map(Result::unwrap).product())
         .collect::<Vec<_>>()[..2] else { unreachable!() };
-    times.split_whitespace()
-        .map(str::parse)
-        .map(Result::unwrap)
-        .zip(dists.split_whitespace()
-             .map(str::parse)
-             .map(Result::unwrap))
+
+    (times, dists)
 }
 
-fn quadratic_roots(a: f32, b: f32, c: f32) -> (f32, f32) {
-    let doublea = 2. * a;
-    let discr = b.powi(2) - 4. * a * c;
+fn quadratic_roots<A>(a: A, b: A, c: A) -> (A, A)
+where A: Float + Mul<A, Output = A> + Zero + Signed + FromPrimitive
+{
+    let doublea = a * A::from_i8(2).unwrap();
+    let discr = b * b - a * c * A::from_i8(4).unwrap();
 
-    assert!(discr > 0., "Discriminant less than zero");
+    assert!(discr > A::zero(), "Discriminant less than zero");
 
     let mid = discr.sqrt() / doublea.abs();
 
     (-b/doublea - mid, -b/doublea + mid)
 }
+
