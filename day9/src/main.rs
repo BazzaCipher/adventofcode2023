f01eb69f4082ed1e094cad94fb6b44fa6495ea1d
BazzaCipher <33623276+BazzaCipher@users.noreply.github.com>
2023-12-27 21:17:40 +1100

    Day 9 Part 1 All

---
+++ day9/src/main.rs
@@ -0,0 +1,41 @@
+#![feature(iter_map_windows)]
+
+use itertools::multipeek;
+
+fn main() {
+    let input = input();
+
+    let out = input.into_iter()
+        .map(|x| build(&x))
+        .map(|x| x.into_iter().map(|y| {
+            *y.last().unwrap()
+        }).sum::<i64>()).sum::<i64>();
+
+    println!("{:?}", out);
+}
+
+fn input() -> Vec<Vec<i64>> {
+    let input = include_str!("../input");
+
+    input.lines()
+        .map(|line|
+            line.split_whitespace()
+                .map(|s| s.parse::<i64>().unwrap())
+                .collect()
+            )
+        .collect()
+}
+
+fn build(set: &[i64]) -> Vec<Vec<i64>> {
+    let mut out = Vec::new();
+    out.push(Vec::from(set));
+
+    let mut last = &out[0];
+    while last.iter().any(|&s| s != 0) {
+        out.push(last.iter().map_windows(|[a, b]| *b - *a).collect());
+        last = out.last().unwrap();
+    }
+
+    out
+}
+
